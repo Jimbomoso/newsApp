80312d3e2fa75819c66faa05a43dfadadc76532f
Jimbomoso <jimbomoso@gmail.com>
2019-08-02 15:14:05 -0400

    redid server file

--- server.js
+++ server.js
@@ -1,172 +1,34 @@
-// Dependencies
-var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const logger = require('morgan');
 
-// Requiring our Note and Article models
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
+const express = require('express');
+const app = express();
 
-// Our scraping tools
-var request = require("request");
-var cheerio = require("cheerio");
-
-// Set mongoose to leverage built in JavaScript ES6 Promises
-mongoose.Promise = Promise;
-
-// Initialize Express
-var app = express();
-
-// Use morgan and body parser with our app
 app.use(logger("dev"));
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
-// Requiring Handlebars
-let exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
-app.set("view engine", "handlebars");
+app.use(express.static(process.cwd() + "/public"));
 
-// Make public a static dir
-app.use(express.static("public"));
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main'})
+);
 
-// Database configuration wiht mongoose
-mongoose.connect("https://pacific-anchorage-70357.herokuapp.com/");
-var db = mongoose.connection;
+app.set('view engine', 'handlebars');
 
-// Show any mongoose errors
-db.on("error", function(error) {
-	console.log("Mongoose Error: ", error);
+mongoose.connect('mongodb://localhost/newsApp');
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Connected to mongoose');
 });
 
-// Once logged in to the db through mongoose, log a success message
-db.once("open", function() {
-	console.log("Mongoose connection successful.");
-});
-
-// Routes
-// A GET request to scrape the echojs website
-// 
-app.get("/", function(req, res) {
-  // Grab every doc in the Articles array
-  Article.find({}, function(error, doc) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    }
-    // Or send the doc to the browser as a json object
-    else {
-      //res.json(doc);
-      res.render("index", {articles: doc});
-    }
-  });
-});
-
-// A GET request to scrape the echojs website
-app.get("/scrape", function(req, res) {
-	// First, we grab the body of the html with request
-	request("http://nypost.com/sports/", function(error, response, html) {
-		// Then, we load that into cheerio and save it to $ for a shorthand selector
-		var $ = cheerio.load(html);
-		// Now, we grab every h2 within an article tag, and do the following:
-		$("article h3").each(function(i, element) {
-			// Save an empty result object
-			var result = {};
-
-			// Add the text and href of every link, and save them as properties of the result object
-			result.title = $(this).children("a").text();
-			result.link = $(this).children("a").attr("href");
-
-			// Using our Article model, create a new entry
-			// This effectively passes the result object to the entry (and the title and link)
-			var entry = new Article(result);
-
-			// Now, save that entry to the db
-			entry.save(function(err, doc) {
-				// Log any errors
-				if (err) {
-					console.log(err);
-				}
-				// Or log the doc
-				else {
-					console.log(doc);
-				}
-			});
-		});
-	});
-	// Tell the browser that we finished scraping the text
-	// res.send("Scrape Complete");
-	res.redirect("/");
-});
-
-// This will get the articles we scraped from the mongoDB
-app.get("/articles", function(req, res) {
-
-	// Grabs all of the articles array
-	Article.find({}, function(error, doc) {
-		// Log any errors
-		if (error) {
-			console.log(error);
-		}
-		// Otherwise, send the doc to the browser as a json object
-		else {
-			res.json(doc);
-		}
-	});
-});
-
-// Grab an article by it's ObjectId
-app.get("/articles/:id", function(req, res) {
-	// Using the id passed in the id parameter, prepare a query that finds the matching one in our db.
-	Article.findOne({ "_id": req.params.id })
-	// .. and populate all of the notes associated with it
-	.populate("note")
-	// now, execute our query
-	.exec(function(error, doc) {
-		// Log any errors
-		if (error) {
-			console.log(error);
-		}
-		// Otherwise, send the doc to the browser as a json object
-		else {
-			res.json(doc);
-		}
-	});
-});
-
-// Create a new note or replace an existing note
-app.post("/articles/:id", function(req, res) {
-	// Create a new note and pass the req.body to the entry
-	var newNote = new Note(req.body);
-
-	// And save the new note the db
-	newNote.save(function(error, doc) {
-		// Log any errors
-		if (error) {
-			console.log(error);
-		}
-		// Otherwise
-		else {
-			// Use the article id to find and update it's note
-			Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
-			// Execute the above query
-			.exec(function(err, doc) {
-				// Log any errors
-				if (err) {
-					console.log(err);
-				}
-				else {
-					// Or send the document to the browser
-					res.send(doc);
-				}
-			});
-		}
-	});
-});
-
-// Listen on port 3000
-app.listen(process.env.PORT || 3000, function() {
-	console.log("App running on port 3000.");
+let port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
 });
