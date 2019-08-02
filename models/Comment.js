c41e832f030a338deb239b31f191842ff81ba12b
Jimbomoso <jimbomoso@gmail.com>
2019-08-02 15:20:58 -0400

    updated comment js file

--- models/Comment.js
+++ models/Comment.js
@@ -1,30 +1,26 @@
 // Require mongoose
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 // Create Schema class
-var Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 // Create article schema
-var ArticleSchema = new Schema({
+const CommentSchema = new Schema({
 	// title is a require string
 	title: {
 		type: String,
 		require: true
 	},
 	// Link is a require string
-	link: {
+	body: {
 		type: String,
 		require: true
 	},
-	// This only saves one note's ObjectId, ref refers to the Note model
-	note: {
-		type: Schema.Types.ObjectId,
-		ref: "Note"
-	}
+	
 });
 
 // Create the Article model with the ArticleSchema
-var Article = mongoose.model("Article", ArticleSchema);
+const Comment = mongoose.model("Comment", CommentSchema);
 
 // Export the model
-module.exports = Article;
+module.exports = Comment;
